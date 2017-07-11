b71b574a058fad4f711feb3eb8ec993468feb070
wuapnjie <wupanjie0611@gmail.com>
2017-07-11 18:07:12 +0800

    upload recycler kit

--- app/src/main/java/com/xiaopo/flying/kits/TextBinder.kt
+++ app/src/main/java/com/xiaopo/flying/kits/TextBinder.kt
@@ -1,27 +1,21 @@
 package com.xiaopo.flying.kits
 
-import android.support.v7.widget.RecyclerView.ViewHolder
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import com.xiaopo.flying.kits.TextBinder.TextViewHolder
+import com.xiaopo.flying.recyclerkit.AnotherViewHolder
 import com.xiaopo.flying.recyclerkit.ItemBinder
-import kotlinx.android.synthetic.main.item_text.view.tv_text
+import kotlinx.android.synthetic.main.item_text.view.*
 
 /**
  * @author wupanjie
  */
-class TextBinder : ItemBinder<String, TextViewHolder>() {
-  override fun createViewHolder(inflater: LayoutInflater, parent: ViewGroup): TextViewHolder {
+class TextBinder : ItemBinder<String, AnotherViewHolder> {
+  override fun createViewHolder(inflater: LayoutInflater, parent: ViewGroup): AnotherViewHolder {
     val itemView = inflater.inflate(R.layout.item_text, parent, false)
-    return TextViewHolder(itemView)
+    return AnotherViewHolder(itemView)
   }
 
-  override fun bindViewHolder(holder: TextViewHolder, item: String) {
-    holder.itemView.tv_text.text = "HHHHHH + ${position}"
-  }
-
-  class TextViewHolder(itemView: View) : ViewHolder(itemView) {
-
+  override fun bindViewHolder(holder: AnotherViewHolder, item: String) {
+    holder.itemView.tv_text.text = "HHHHHH + ${holder.adapterPosition}"
   }
 }
